8f56aa4cd2eb821abd23a11f70036be762d3f4a9
Krishna <krishna.verma@nucleusteq.com>
2023-06-15 17:55:13 +0530

    deleted files

--- cartServices/src/main/java/com/cart/services/repository/CartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.cart.services.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.cart.services.dbo.CartItem;
-
-public interface CartRepository extends MongoRepository<CartItem, Long> {
-
-}
--- productService/src/main/java/com/product/services/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.product.services.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.product.services.dbo.Product;
-
-public interface ProductRepository extends MongoRepository<Product, Long>{
-
-}
